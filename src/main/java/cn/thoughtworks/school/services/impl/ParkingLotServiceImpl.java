44f2839bca6231f1cd474dcd410603ca335921a1
Qian Zhang <897157658@qq.com>
2022-10-31 14:03:10 +0800

    feat ：创建assignParkingLot 的service 层.

--- src/main/java/cn/thoughtworks/school/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/cn/thoughtworks/school/services/impl/ParkingLotServiceImpl.java
@@ -1,7 +1,9 @@
 package cn.thoughtworks.school.services.impl;
 
+import cn.thoughtworks.school.entities.Employee;
 import cn.thoughtworks.school.entities.ParkingLot;
 import cn.thoughtworks.school.entities.dto.createParkingLotRequestDto;
+import cn.thoughtworks.school.repository.EmployeeRepository;
 import cn.thoughtworks.school.repository.ParkingLotRepository;
 import cn.thoughtworks.school.services.ParkingLotService;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class ParkingLotServiceImpl implements ParkingLotService {
 
     ParkingLotRepository parkingLotRepository;
+    EmployeeRepository employeeRepository;
 
     public ParkingLotServiceImpl(ParkingLotRepository parkingLotRepository) {
         this.parkingLotRepository = parkingLotRepository;
@@ -21,6 +24,14 @@ public class ParkingLotServiceImpl implements ParkingLotService {
         ParkingLot parkingLot = ParkingLot.builder()
                 .name(createParkingLotRequestDto.getName())
                 .build();
+        parkingLotRepository.save(parkingLot);
+    }
+
+    @Override
+    public void assignParkingLot(Long parkingLotId, Long employeeId) {
+        ParkingLot parkingLot = parkingLotRepository.findById(parkingLotId).orElseThrow(null);
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(null);
+        parkingLot.setEmployee(employee);
         parkingLotRepository.save(parkingLot);
     }
 }
